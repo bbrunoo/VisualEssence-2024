c0868e701b959f01ae773fda17ee622f459cde8b
AnaMorata8642 <anacarolinasantosmorata@gmail.com>
2024-11-20 20:58:01 -0300

    Update_PaisHome

--- Application/src/app/Pages/PaisHome/jogos/jogos.component.ts
+++ Application/src/app/Pages/PaisHome/jogos/jogos.component.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from "../Shared-Pais/header/header.component";
+import { FontSizeService } from '../../Font/font-size.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-jogos',
   standalone: true,
-  imports: [RouterLink, HeaderComponent],
+  imports: [RouterLink, HeaderComponent, NgClass],
   templateUrl: './jogos.component.html',
   styleUrl: './jogos.component.css'
 })
 export class JogosComponent {
   Dalt: string = '../../../assets/HomeImages/daltonismo.png';
   Miop: string = '../../../assets/HomeImages/miopia.png';
+
+  //------------------------------------------------------------------------------------//
+
+  constructor(public fontSizeService: FontSizeService) { }
+
+  ngOnInit(): void {
+    this.fontSizeService.initializeFontSize('txH1', 25);
+    this.fontSizeService.initializeFontSize('txJog', 20);
+  }
+
+  getFontSizeClass(): string {
+    if (this.fontSizeService.fontSizeMultiplier > 1.2) {
+      return 'size1_2';
+    }
+    return '';
+  }
+
+  aumentarFonte() {
+    this.fontSizeService.increaseFontSize();
+  }
+
+  diminuirFonte() {
+    this.fontSizeService.decreaseFontSize();
+  }
 }
