fc3d4a530e298523d25dccbbbeaa67395a73b5a0
Bruno <Bruno@DESKTOP-AA6HRQC>
2024-11-22 23:18:24 -0300

    final fixes in "sobre-nos"

--- Application/src/app/Pages/PaisHome/sobre-nos/sobre-nos.component.ts
+++ Application/src/app/Pages/PaisHome/sobre-nos/sobre-nos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../Shared-Pais/footer/footer.component';
+import { LogoMenuComponent } from '../../SharedMenu/logo-menu/logo-menu.component';
 
 @Component({
   selector: 'app-sobre-nos',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, LogoMenuComponent],
   templateUrl: './sobre-nos.component.html',
   styleUrl: './sobre-nos.component.css'
 })
